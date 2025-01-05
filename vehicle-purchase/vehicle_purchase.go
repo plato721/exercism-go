5154279ee6ee2f92401c60dbfff2ac57570925e1
Matt Ewell <matt.ewell@gmail.com>
2025-01-05 14:18:22 -0700

    Rename some things

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -2,32 +2,32 @@ package purchase
 
 import "fmt"
 
-var licenseRequiredVehicles = [2]string{"car", "truck"}
+var vehiclesNeedingLicense = [2]string{"car", "truck"}
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	licenseRequired := false
-	for i := 0; i < len(licenseRequiredVehicles); i++ {
-		if kind == licenseRequiredVehicles[i] {
-			licenseRequired = true
+	needsLicense := false
+	for i := 0; i < len(vehiclesNeedingLicense); i++ {
+		if kind == vehiclesNeedingLicense[i] {
+			needsLicense = true
 			break
 		}
 	}
-	return licenseRequired
+	return needsLicense
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
 	suffix := "is clearly the better choice."
-	var betterChoice string
+	var bestOption string
 
 	if option1 < option2 {
-		betterChoice = option1
+		bestOption = option1
 	} else {
-		betterChoice = option2
+		bestOption = option2
 	}
 
-	return fmt.Sprint(betterChoice, " ", suffix)
+	return fmt.Sprint(bestOption, " ", suffix)
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
